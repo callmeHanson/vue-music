41c538e2a8635a2e7decc9c37ff8a56ae9a78a01
Hang Wu <hanosncoder@gmail.com>
2020-02-02 16:44:01 +0800

    console

--- src/main.js
+++ src/main.js
@@ -6,12 +6,14 @@ import '@/common/less/index.less'
 // 去除 移动端300ms检测双击的 延迟
 import Fastclick from 'fastclick'
 import VueLazyload from 'vue-lazyload'
-/* eslint-disable no-unused-vars */
+
 import Vconsole from "vconsole"
+/* eslint-disable no-unused-vars */
+const vConsole = new Vconsole();
 
 console.log("test")
 
-Vue.config.productionTip = false
+// Vue.config.productionTip = false
 
 Fastclick.attach(document.body)
 
